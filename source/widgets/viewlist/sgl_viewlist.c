7a9aaf5301f5dc62494bbd45282ac8e9a75e67c4
LiShanwenGit <1477153217@qq.com>
2026-05-22 23:15:11 +0800

    fix: viewlist widget issue

--- source/widgets/viewlist/sgl_viewlist.c
+++ source/widgets/viewlist/sgl_viewlist.c
@@ -88,7 +88,7 @@ static void sgl_viewlist_construct_cb(sgl_surf_t *surf, sgl_obj_t* obj, sgl_even
         }
         else if((viewlist->pos_y + diff) < list_h) {
             viewlist->pos_y = list_h - diff + viewlist->margin_y;
-            pos_y = viewlist->pos_y - 2 * viewlist->margin_y + obj->border + viewlist->margin_y;
+            pos_y = viewlist->pos_y - viewlist->margin_y + obj->border - 1;
             sgl_obj_for_each_child(child, obj) {
                 sgl_obj_set_pos_y(child, pos_y);
                 pos_y += (viewlist->item_height + viewlist->margin_y);
